a18fd9655835dd5ef58951d0e3cdf3a66968e517
abreu-dev <gabrielabreu.dev@gmail.com>
2021-11-05 18:09:08 -0300

    Testes unitários AppService, CommandHandler e Command

---
+++ tests/GenericImporter.Application.Tests/Services/XptoAppServiceTests.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using GenericImporter.Application.DataTransferObjects.XptoDTOs;
+using GenericImporter.Application.Interfaces;
+using GenericImporter.Application.Services;
+using GenericImporter.Domain.Commands.XptoCommands;
+using GenericImporter.Domain.Core.Mediator;
+using GenericImporter.Domain.Entities;
+using GenericImporter.Domain.Interfaces;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GenericImporter.Application.Tests.Services
+{
+    public class XptoAppServiceTests
+    {
+        private readonly Mock<IMapper> _mockMapper;
+        private readonly Mock<IMediatorHandler> _mockMediatorHandler;
+        private readonly IXptoAppService _xptoAppService;
+
+        public XptoAppServiceTests()
+        {
+            _mockMapper = new Mock<IMapper>();
+            _mockMediatorHandler = new Mock<IMediatorHandler>();
+            _xptoAppService = new XptoAppService(
+                _mockMapper.Object,
+                _mockMediatorHandler.Object,
+                new Mock<IXptoRepository>().Object);
+        }
+
+        [Fact]
+        [Trait("AppService", "Xpto")]
+        public async Task Add_ShouldPublishAddXptoCommand_Ever()
+        {
+            // Arrange
+            var addXptoDTO = new AddXptoDTO()
+            {
+                Name = "Xpto"
+            };
+
+            var command = new AddXptoCommand()
+            {
+                Entity = new Xpto()
+                {
+                    Name = addXptoDTO.Name
+                }
+            };
+
+            _mockMapper.Setup(e => e.Map<AddXptoCommand>(It.Is<AddXptoDTO>(s => s.Equals(addXptoDTO)))).Returns(command);
+
+            // Act
+            await _xptoAppService.Add(addXptoDTO);
+
+            // Assert
+            _mockMediatorHandler.Verify(e => e.SendCommand(It.Is<AddXptoCommand>(s => s.Equals(command))), Times.Once);
+        }
+    }
+}
