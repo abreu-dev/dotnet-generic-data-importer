4330dfbb207476fe0536bc4d1ddf6ebab90b77d0
abreu-dev <gabrielabreu.dev@gmail.com>
2021-11-06 17:33:03 -0300

    Removendo mensagens não utilizadas

--- backend/src/GenericImporter.Domain.Core/Common/DomainMessages.cs
+++ backend/src/GenericImporter.Domain.Core/Common/DomainMessages.cs
@@ -5,8 +5,5 @@
         public static DomainMessage CommitFailed => new("There was an error saving data.");
         public static DomainMessage RequiredField => new("Please, ensure you enter {0}.");
         public static DomainMessage AlreadyInUse => new("The informed {0} is already in use.");
-        public static DomainMessage InvalidFormat => new("The informed {0} is invalid.");
-        public static DomainMessage NotFound => new("The informed {0} was not found.");
-        public static DomainMessage InUseByAnotherEntity => new("The informed {0} is in use by {1}.");
     }
 }
