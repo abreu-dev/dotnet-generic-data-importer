02efe389a9ea99e5912fbaad39e9f9aff0cf05ab
gabriel.roberto.abreu@gmail.com <gabriel.roberto.abreu@gmail.com>
2021-12-31 15:40:18 -0300

    Ajustes sonar

--- backend/src/Something.Application/Interfaces/IXptoAppService.cs
+++ backend/src/Something.Application/Interfaces/IXptoAppService.cs
@@ -1,11 +1,9 @@
 ﻿using Core.Application.Interfaces;
 using Something.Application.DataTransferObjects.XptoDtos;
-using System.Threading.Tasks;
 
 namespace Something.Application.Interfaces
 {
     public interface IXptoAppService : IAppService<XptoDto, AddXptoDto> 
     {
-        Task Import(string file);
     }
 }
